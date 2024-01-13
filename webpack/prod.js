3c62ccdefdd7d7633feb72fe698d473e2a9acd77
PachyuChepe <pachyuchepe@gmail.com>
2024-01-13 17:16:35 +0900

    Fix: 배포테스트

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,27 +1,50 @@
-const merge = require("webpack-merge");
-const path = require("path");
-const base = require("./base");
-const TerserPlugin = require("terser-webpack-plugin");
+// const merge = require("webpack-merge");
+// const path = require("path");
+// const base = require("./base");
+// const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(base, {
-  mode: "production",
+// module.exports = merge(base, {
+//   mode: "production",
+//   output: {
+//     filename: "bundle.min.js"
+//   },
+//   devtool: false,
+//   performance: {
+//     maxEntrypointSize: 900000,
+//     maxAssetSize: 900000
+//   },
+//   optimization: {
+//     minimizer: [
+//       new TerserPlugin({
+//         terserOptions: {
+//           output: {
+//             comments: false
+//           }
+//         }
+//       })
+//     ]
+//   }
+// });
+
+// webpack/prod.js
+const { merge } = require('webpack-merge');
+const path = require('path');
+const baseConfig = require('./base');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
   output: {
-    filename: "bundle.min.js"
-  },
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.min.js'
   },
   optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    })],
+  },
 });
