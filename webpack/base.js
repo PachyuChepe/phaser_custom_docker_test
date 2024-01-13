7373b7a088639795cd5982dc5a9a4caef8d5af36
PachyuChepe <pachyuchepe@gmail.com>
2024-01-13 17:40:18 +0900

    Fix: 배포테스트

--- webpack/base.js
+++ webpack/base.js
@@ -36,5 +36,19 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html"
     })
-  ]
+  ],
+  devServer: {
+    // 모든 호스트에서의 접근을 허용
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  }
+    // devServer 설정 추가
+    // devServer: {
+    //   host: '0.0.0.0', // 모든 네트워크 인터페이스에서 들어오는 요청을 수락
+    //   port: 8080, // 사용할 포트 번호 (선택적)
+    //   disableHostCheck: true, // 호스트 체크 비활성화
+    //   contentBase: path.join(__dirname, "public"), // 정적 파일 경로 (선택적)
+    //   publicPath: '/', // 번들에 접근하기 위한 경로 (선택적)
+    //   hot: true // 핫 모듈 교체 활성화 (선택적)
+    // }
 };
